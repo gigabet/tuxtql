f70e18aa94b5e57a75fc37b5c5ba8adf47339726
Summer <47139205+gigabet@users.noreply.github.com>
2022-04-14 10:35:22 +0300

    remove babel, update yarn, upgrade packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,1 @@
 /** @type {import('next').NextConfig} */
-const withTM = require('next-transpile-modules')(['@mui/material', '@mui/icons-material']) // pass the modules you would like to see transpiled
-
-module.exports = withTM({
-  reactStrictMode: true,
-})
